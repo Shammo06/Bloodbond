c4dfaedd40a434167b88f935fd8654dfbf113240
Hasan9955 <hasanbinali7556@gmail.com>
2024-01-18 23:36:57 +0600

    update mission page

--- src/Pages/Mission.tsx
+++ src/Pages/Mission.tsx
@@ -4,6 +4,8 @@ const Mission = () => {
     return (
         <div>
             This is our mission and vision page!!!
+            <h1>Our Mission</h1>
+            <p>To enhance the well being of patients in our service area by assuring a reliable and economical supply of the safest possible blood, by providing innovative hemotherapy services, and by promoting research and education programs in transfusion medicine.</p>
         </div>
     );
 };
