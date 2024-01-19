1543aa782f79de13a7d4fc7ab766b0ee27a43657
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2024-01-19 20:44:14 +0600

    router added

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Banner from "../../Component/Banner/Banner";
+import Statistics from "../../Component/Statistics/Statistics";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Statistics></Statistics>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App';
-import About from './Pages/About/About';
-import Mission from './Pages/Mission';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import About from "./Pages/About/About";
+import Mission from "./Pages/Mission";
+import Layout from "./Pages/Layout/Layout";
+import Home from "./Pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/mission",
+        element: <Mission></Mission>,
+      },
+    ],
   },
-  {
-    path: "/about",
-    element: <About></About>
-  },
-  {
-    path: '/mission',
-    element: <Mission></Mission>
-  }
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
