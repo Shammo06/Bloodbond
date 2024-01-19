71a740ac54ac910d1734b7090a6931983d352c9e
abusaleh2234 <you@example.com>
2024-01-19 14:29:36 +0600

    about page add in project

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from './App';
+import About from './Pages/About/About';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
   },
+  {
+    path: "/about",
+    element: <About></About>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
