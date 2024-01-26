b14c659116ead8f2eb04afaaeddd98aea362a33d
Shammo06 <mrshammo2018@gmail.com>
2024-01-26 13:06:20 +0600

    Donation req show in home page

--- src/Component/RequestBlood/RequestBlood.tsx
+++ src/Component/RequestBlood/RequestBlood.tsx
@@ -1,32 +1,47 @@
-import { useEffect } from "react";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+interface FormData {
+    patientName: string;
+    bloodGroup: string;
+    bloodBag: number;
+    time: string;
+    location: string;
+    phone: number;
+    _id: string;
+  }
 
 const RequestBlood = () => {
-    const [request, setRequest] = useState<any[]>([])
+    const [request, setRequest] = useState<FormData[]>([])
 
     useEffect(() => {
-        fetch('/Donors.json')
-            .then(res => res.json())
+        axios.get('https://blood-bound.vercel.app/getbloodrequests')
             .then(data => {
-                setRequest(data)
+                setRequest(data.data.bloodRequests)
+                console.log(data.data)
             })
 
     }, [])
     return (
         <div>
-            <div className="">
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
-                <h2 className="card-title">A+ Blood Needed</h2>
-                <p>Patient Name: xxxxxx</p>
-                <p>Blood Group: xxxxxx</p>
-                <p>Donation Time: xxxxxx</p>
-                <p>Location: xxxxxx</p>
-                <p>Contact: xxxxxx</p>
-                <div className="card-actions justify-end">
-                <button className="btn btn-primary">Donate Blood</button>
-                </div>
-            </div>
+            <div className="px-20 py-5">
+                <p className='text-4xl text-center py-5'>Donate Your Blood</p>
+            <div className="grid grid-cols-2 gap-5">
+                {
+                    request?.slice(0,2).map(data => 
+                    <div key={data._id} className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">{data.bloodGroup} Blood Needed</h2>
+                        <p>Patient Name: {data.patientName}</p>
+                        <p>Blood Need: {data.bloodBag} Bag</p>
+                        <p>Donation Time: {data.time}</p>
+                        <p>Location: {data.location}</p>
+                        <p>Contact: {data.phone}</p>
+                        <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Donate Blood</button>
+                        </div>
+                    </div>
+                    </div>)
+                }
             </div>
             </div>
         </div>
