c462879abdf9069b530cdec36f7955bd3d447af2
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2024-01-26 20:05:04 +0600

    blood section added

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import Banner from "../../Component/Banner/Banner";
+import BloodSection from "../../Component/BloodSection/BloodSection";
 import RequestBlood from "../../Component/RequestBlood/RequestBlood";
 import Statistics from "../../Component/Statistics/Statistics";
 import WhyDonateBlood from "../../Component/WhyDonateBlood/WhyDonateBlood";
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div className="space-y-5 md:mt-5">
       <Banner></Banner>
+      <BloodSection></BloodSection>
       <WhyDonateBlood></WhyDonateBlood>
       <Statistics></Statistics>
       <RequestBlood></RequestBlood>
