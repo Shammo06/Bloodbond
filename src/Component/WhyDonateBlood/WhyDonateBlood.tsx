97c0b7e4e738f6f76f3eb4de70998911f19b2e8b
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-01-26 16:52:05 +0600

    created whyDonateBlood section

--- src/Component/WhyDonateBlood/WhyDonateBlood.tsx
+++ src/Component/WhyDonateBlood/WhyDonateBlood.tsx
@@ -1,8 +1,29 @@
 const WhyDonateBlood = () => {
   return (
     <div>
-      <div className="container mx-auto">
-        <h3 className="text-center text-3xl font-bold">Why Donate Blood</h3>
+      <div className="container mx-auto py-32">
+        <div className="mb-14">
+          <h3 className="text-center text-3xl font-bold mb-4">
+            Why Donate Blood
+          </h3>
+          <p className="text-center  max-w-[700px] mx-auto text-lg px-5 lg:px-0">
+            Be a hero in someone's life – donate blood. Your simple act of
+            kindness can save lives and provide hope. Discover the compelling
+            reasons behind blood donation and join us in making a difference
+            today.
+          </p>
+        </div>
+        <div>
+          {/* div for img */}
+          <div>
+            <img
+              src={"https://i.ibb.co/379Mhqw/donor-purple-circle.png"}
+              alt="Img of Blood Donor"
+            />
+          </div>
+          {/* div for texts */}
+          <div></div>
+        </div>
       </div>
     </div>
   );
