10f1367b88f4497aef05d540a19ab21377842018
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-02-01 19:41:34 +0600

    update login and registration form responsiveness

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -59,7 +59,7 @@ const Login: React.FC = () => {
     >
       <div
         style={{ boxShadow: "0px 3px 14px 6px rgba(0,0,0,0.28)" }}
-        className="card-body rounded-lg w-3/4 2xl:w-3/5 mx-auto bg-white py-16"
+        className="card-body rounded-lg w-[95%] sm:w-3/4 2xl:w-3/5 mx-auto bg-white py-16"
       >
         <h3 className="text-center text-4xl font-bold mb-8">Please Login</h3>
         <Formik
