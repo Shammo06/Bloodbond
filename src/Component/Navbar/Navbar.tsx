13e747723166ce1eb344d5a98f22293384a6b34e
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2024-02-02 20:43:51 +0600

    navbar position changed

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -64,7 +64,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className=" navbar fixed z-50 bg-[#DC0000] text-white font-semibold">
+    <div className=" navbar  bg-[#DC0000] text-white font-semibold">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
