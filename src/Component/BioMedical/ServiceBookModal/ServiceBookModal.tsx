ab86e4ec906153ac922f0225a46d709a0e8fbc2e
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-02-06 23:29:13 +0600

    created booking form and get value

--- src/Component/BioMedical/ServiceBookModal/ServiceBookModal.tsx
+++ src/Component/BioMedical/ServiceBookModal/ServiceBookModal.tsx
@@ -1,4 +1,33 @@
-const ServiceBookModal = () => {
+import useAuth from "../../../hooks/useAuth";
+
+const ServiceBookModal: React.FC = () => {
+  const auth = useAuth();
+
+  if (!auth) {
+    return;
+  }
+
+  const { user } = auth;
+  if (!user) {
+    return null;
+  }
+
+  const { displayName, email } = user;
+
+  const handleSubmit = () => {
+    document.getElementById("submit")?.click();
+  };
+
+  const handleBookingForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const userName = formData.get("name") as string;
+    const userEmail = formData.get("email") as string;
+    const date = formData.get("date") as string;
+    console.log("Form submitted", userName, userEmail, date);
+  };
+
   return (
     <div>
       <button
@@ -16,12 +45,72 @@ const ServiceBookModal = () => {
       </button>
       <dialog id="my_modal_4" className="modal">
         <div className="modal-box w-11/12 max-w-5xl">
-          <h3 className="font-bold text-lg">Service Booking</h3>
-          <p className="py-4">Click the button below to close</p>
-          <div className="modal-action">
-            <form method="dialog">
-              <button className="btn">Close</button>
+          <div className="flex items-center justify-between">
+            <h3 className="font-bold text-lg">Service Booking</h3>
+            <form method="dialog" className="">
+              <button className="btn btn-sm btn-circle btn-ghost">✕</button>
             </form>
+          </div>
+          <div className="divider m-0"></div>
+          {/* modal body */}
+          <div>
+            <form
+              style={{ boxShadow: "0px 4px 10px 5px rgba(167,167,167,0.3)" }}
+              onSubmit={handleBookingForm}
+              className="p-4 rounded-lg my-5"
+            >
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  required
+                  readOnly
+                  value={displayName ?? ""}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                  readOnly
+                  value={email ?? ""}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Date</span>
+                </label>
+                <input
+                  type="date"
+                  name="date"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <button id="submit" type="submit"></button>
+            </form>
+          </div>
+          <div className="divider m-0"></div>
+          {/* purchase button */}
+          <div className="flex justify-end">
+            <button
+              onClick={handleSubmit}
+              id="submit"
+              className="btn btn-outline bg-[#EA062B] text-white"
+            >
+              Book This Service
+            </button>
           </div>
         </div>
       </dialog>
