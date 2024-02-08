04399982a326cf965357b3cbf8b70324a98236c5
Shammo06 <mrshammo2018@gmail.com>
2024-02-09 00:45:50 +0600

    modify donor search

--- src/Component/DonorCard/DonorCard.tsx
+++ src/Component/DonorCard/DonorCard.tsx
@@ -1,44 +1,44 @@
 import { FaFacebookMessenger } from "react-icons/fa";
 
-interface Donor {
+interface data {
     name: string;
     email: string;
-    photo: string;
-    phone: string;
-    district: string;
-    bloodGroup: string;
-    upazila: string;
-    lastTimeDonate: string;
-    address: string;
+    photo: string;    
+    donor: {
+        phone: string;
+        district: string;
+        bloodGroup: string;
+        upazila: string;
+        address: string;
+    };
 }
+
+
+
 
 interface DonorCardProps {
-    donor: Donor;
+    data: data;
 }
 
-const DonorCard: React.FC<DonorCardProps> = ({ donor }) => {
+const DonorCard: React.FC<DonorCardProps> = ({ data }) => {
     const {
         name, 
-        phone, 
-        bloodGroup,
-        photo,
-        district,
-        upazila, 
+        donor,
+        photo     
 
-    } = donor;
+    } = data;
 
-    // const { userName, email, phone, lastTimeDonate,  bloodGroup, photo, district, subDistrict, address } = donor;
-
+    console.log(photo)
     return (
         <div>
             <div className="card bg-[#EB2C2926] shadow-xl text-black">
                 <figure><img className="w-32 h-32 rounded-full mt-4" src={photo} alt="Donor" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Name: {name}</h2>
-                    <p>Blood Group: {bloodGroup}</p>
-                    <p>Phone: {phone}</p>
-                    <p>District: {district}</p>
-                    <p>Upazila: {upazila}</p>
+                    <p>Blood Group: {donor.bloodGroup}</p>
+                    <p>Phone: {donor.phone}</p>
+                    <p>District: {donor.district}</p>
+                    <p>Upazila: {donor.upazila}</p>
 
                     <div className="card-actions justify-end">
                         <button className="btn bg-[#EB2C29] text-white"><FaFacebookMessenger /> Chat Now</button>
