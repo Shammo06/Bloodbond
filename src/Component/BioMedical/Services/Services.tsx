56a6f2ba49312818609610787615b900abb07d6e
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-02-11 18:53:45 +0600

    added loading to the bioMedical services page

--- src/Component/BioMedical/Services/Services.tsx
+++ src/Component/BioMedical/Services/Services.tsx
@@ -19,12 +19,21 @@ const Services: React.FC = () => {
         <h3 className="font-bold text-center text-4xl mb-16">
           Our Bio-Medical Services
         </h3>
+
         {/* services */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {allBioMedicalServices.map((service: Service) => (
-            <ServiceCard key={service._id} service={service}></ServiceCard>
-          ))}
-        </div>
+        {allBioMedicalServices.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {allBioMedicalServices?.map((service: Service) => (
+              <ServiceCard key={service._id} service={service}></ServiceCard>
+            ))}
+          </div>
+        ) : (
+          <div className="container mx-auto py-8">
+            <div className="flex justify-center items-center">
+              <span className="loading loading-spinner loading-lg"></span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
