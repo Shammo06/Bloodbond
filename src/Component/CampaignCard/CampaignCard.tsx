548815a832982637325b0efb2c0d05047995681f
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-02-12 22:02:59 +0600

    make campaignCard dynamic

--- src/Component/CampaignCard/CampaignCard.tsx
+++ src/Component/CampaignCard/CampaignCard.tsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-import group from "../../assets/cm1.jpg";
+// import { Link } from "react-router-dom";
 import { Campaign } from "../../Pages/Campaign/UpcomingCampaigns/UpcomingCampaigns";
 import React from "react";
 
@@ -8,28 +7,57 @@ interface CampaignCardProps {
 }
 
 const CampaignCard: React.FC<CampaignCardProps> = ({ campaign }) => {
-  console.log(campaign);
+  const {
+    campaignImgUrl,
+    campaignTitle,
+    district,
+    subDistrict,
+    description,
+    startDate,
+  } = campaign;
+
+  const wordsPerDescription = 18;
+  const slicedDescription = description
+    .split(" ")
+    .slice(0, wordsPerDescription)
+    .join(" ");
+
+  const [, month, date] = startDate.split("-");
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
   return (
-    <Link to="/campaignDetails">
-      <div className="bg-slate-100 p-4 max-w-[540px] space-y-2 rounded-xl duration-500 border-b-4 border-white  hover:border-[#ea062b]">
-        <div className="relative">
-          <img className="h-[210px] w-full" src={group} alt="" />
-          <div className="absolute top-0 left-0  bg-[#ea062b] py-3 px-4 text-white">
-            <h4 className="text-lg font-bold">12</h4>
-            <h5>Feb</h5>
-          </div>
+    <div className="bg-slate-100 p-4 space-y-2 rounded-xl duration-500 border-b-4 border-[#F1F5F9]  hover:border-[#ea062b]">
+      <div className="relative">
+        <img className="h-[210px] w-full" src={campaignImgUrl} alt="" />
+        <div className="absolute top-0 left-0  bg-[#ea062b] py-3 px-4 text-white">
+          <h4 className="text-lg font-bold">{date}</h4>
+          <h5>{months[parseInt(month) - 1]}</h5>
         </div>
-        <address>Dhanmondi, Dhaka</address>
-        <h2 className="text-2xl font-bold text-[#ea062b]">
-          LifeSaver Blood Donation Drive
-        </h2>
-        <p>
-          Blood group collection involves determining an individual's blood type
-          to facilitate safe blood transfusions and medical procedures.
-        </p>
       </div>
-    </Link>
+      <address>
+        {district}, {subDistrict}
+      </address>
+      <h2 className="text-2xl font-bold text-[#ea062b]">{campaignTitle}</h2>
+      <p>
+        {description.length > wordsPerDescription
+          ? slicedDescription
+          : description}
+      </p>
+    </div>
   );
 };
 
