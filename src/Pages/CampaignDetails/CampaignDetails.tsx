d5676e61b792038595624b8f3e0d2785d5a283d9
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-02-21 23:09:57 +0600

    added loader to the campaignDetails page

--- src/Pages/CampaignDetails/CampaignDetails.tsx
+++ src/Pages/CampaignDetails/CampaignDetails.tsx
@@ -19,7 +19,7 @@ const CampaignDetails: React.FC = () => {
     setIsModalOpen(false);
   };
 
-  const { data: specificCampaignAndOthers = {} } = useQuery({
+  const { data: specificCampaignAndOthers = {}, isLoading } = useQuery({
     queryKey: ["specificCampaignAndOthers", id],
     queryFn: async () => {
       const res = await axiosPublic.post("/getspecificcampaignandothers", {
@@ -58,84 +58,94 @@ const CampaignDetails: React.FC = () => {
   ];
 
   return (
-    <div className="container mx-auto grid grid-cols-1 lg:grid-cols-5 gap-6 py-14">
-      <div className="lg:col-span-3 px-5 lg:px-0 lg:pl-5">
-        {specificCampaign && (
-          <>
-            <div className="bg-[#F1F5F9] shadow-lg rounded-xl">
-              <figure className="relative">
-                <img
-                  className="w-full h-auto mx-auto rounded-tl-xl rounded-tr-xl"
-                  src={photo}
-                  alt="campaign"
-                />
-              </figure>
-              <div className="md:mt-5 mb-10 pb-8">
-                <div className="flex flex-col-reverse md:flex-row justify-between md:items-start">
-                  <div className="mt-3 md:mt-0 px-2 md:px-6 xl:px-8">
-                    <h2 className="text-2xl font-bold text-[#DC0000]">
-                      {title}
-                    </h2>
-                    <address>
-                      {district}, {subDistrict}
-                    </address>
-                  </div>
-                  <div className="bg-[#DC0000] text-white px-5 md:py-2 xl:mr-6 md:mr-4 flex-shrink-0">
-                    <h4 className="text-lg font-bold text-center">
-                      {date}-{month && months[parseInt(month) - 1]}-{year}
-                    </h4>
-                  </div>
-                </div>
-                <p className="px-2 md:px-6 xl:px-8 mt-6 text-lg font-medium mb-5">
-                  {description}
-                </p>
-                {/* ------------------- buttons ------------------- */}
-                <div className="flex flex-col md:flex-row gap-5 px-2 md:px-6 xl:px-8">
-                  <button
-                    onClick={openModal}
-                    className="btn btn-outline bg-[#DC0000] text-white hover:text-[#DC0000] hover:bg-white hover:border-[#DC0000] px-9"
-                  >
-                    Register as Volunteer
-                  </button>
-                  <div>
-                    {isModalOpen && specificCampaign && (
-                      <VolunteerRegisterModal
-                        campaign={specificCampaign}
-                        closeModal={closeModal}
-                      ></VolunteerRegisterModal>
-                    )}
-                  </div>
-                  <button className="btn btn-outline bg-[#DC0000] text-white hover:text-[#DC0000] hover:bg-white hover:border-[#DC0000] px-9">
-                    Donate for this Camping!
-                  </button>
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-
-      {/* other campaigns */}
-      <div className="lg:col-span-2 px-5 lg:pl-0">
-        <h3 className="text-center text-2xl font-bold mb-14 text-white">
-          Other Upcoming Campaigns
-        </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
-          {otherCampaigns && otherCampaigns.length > 0 ? (
-            otherCampaigns?.map((campaign: Campaign) => (
-              <CampaignCard
-                key={campaign._id}
-                campaign={campaign}
-              ></CampaignCard>
-            ))
-          ) : (
-            <h6 className="text-center text-white">
-              No other campaigns are available.
-            </h6>
-          )}
+    <>
+      {isLoading ? (
+        <div className="container mx-auto py-40">
+          <div className="flex justify-center items-center">
+            <span className="loading loading-spinner loading-lg text-white"></span>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="container mx-auto grid grid-cols-1 lg:grid-cols-5 gap-6 py-14">
+          <div className="lg:col-span-3 px-5 lg:px-0 lg:pl-5">
+            {specificCampaign && (
+              <>
+                <div className="bg-[#F1F5F9] shadow-lg rounded-xl">
+                  <figure className="relative">
+                    <img
+                      className="w-full h-auto mx-auto rounded-tl-xl rounded-tr-xl"
+                      src={photo}
+                      alt="campaign"
+                    />
+                  </figure>
+                  <div className="md:mt-5 mb-10 pb-8">
+                    <div className="flex flex-col-reverse md:flex-row justify-between md:items-start">
+                      <div className="mt-3 md:mt-0 px-2 md:px-6 xl:px-8">
+                        <h2 className="text-2xl font-bold text-[#DC0000]">
+                          {title}
+                        </h2>
+                        <address>
+                          {district}, {subDistrict}
+                        </address>
+                      </div>
+                      <div className="bg-[#DC0000] text-white px-5 md:py-2 xl:mr-6 md:mr-4 flex-shrink-0">
+                        <h4 className="text-lg font-bold text-center">
+                          {date}-{month && months[parseInt(month) - 1]}-{year}
+                        </h4>
+                      </div>
+                    </div>
+                    <p className="px-2 md:px-6 xl:px-8 mt-6 text-lg font-medium mb-5">
+                      {description}
+                    </p>
+                    {/* ------------------- buttons ------------------- */}
+                    <div className="flex flex-col md:flex-row gap-5 px-2 md:px-6 xl:px-8">
+                      <button
+                        onClick={openModal}
+                        className="btn btn-outline bg-[#DC0000] text-white hover:text-[#DC0000] hover:bg-white hover:border-[#DC0000] px-9"
+                      >
+                        Register as Volunteer
+                      </button>
+                      <div>
+                        {isModalOpen && specificCampaign && (
+                          <VolunteerRegisterModal
+                            campaign={specificCampaign}
+                            closeModal={closeModal}
+                          ></VolunteerRegisterModal>
+                        )}
+                      </div>
+                      <button className="btn btn-outline bg-[#DC0000] text-white hover:text-[#DC0000] hover:bg-white hover:border-[#DC0000] px-9">
+                        Donate for this Camping!
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+
+          {/* other campaigns */}
+          <div className="lg:col-span-2 px-5 lg:pl-0">
+            <h3 className="text-center text-2xl font-bold mb-14 text-white">
+              Other Upcoming Campaigns
+            </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
+              {otherCampaigns && otherCampaigns.length > 0 ? (
+                otherCampaigns?.map((campaign: Campaign) => (
+                  <CampaignCard
+                    key={campaign._id}
+                    campaign={campaign}
+                  ></CampaignCard>
+                ))
+              ) : (
+                <h6 className="text-center text-white">
+                  No other campaigns are available.
+                </h6>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
